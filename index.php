e43d3ed7468feaec4bc12280edfc3fd68e82e8fd
Jae Basagre <basagrejae@gmail.com>
2025-06-10 10:19:34 +0800

    Vtracker app

--- index.php
+++ index.php
@@ -4,59 +4,20 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/reset.css">
-    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/style.css">
+    <script src="track.js"></script>
     <title>VTracker</title>
-    <style>
-    #map {
-      height: 90vh;
-      width: 100%;
-    }
-    </style>
 </head>
-<body>
-  <h2>Live Location Tracker</h2>
-  <div id="map"></div>
-
-  <script>
-    let map, marker;
-
-    function initMap() {
-      const initialPosition = { lat: 0, lng: 0 };
-      map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 15,
-        center: initialPosition
-      });
-
-      marker = new google.maps.Marker({
-        position: initialPosition,
-        map: map,
-        title: "Current Location"
-      });
-
-      updateLocation();
-      setInterval(updateLocation, 5000); // Refresh every 5 seconds
-    }
-
-    function updateLocation() {
-      fetch("get_latest_location.php")
-        .then(response => response.json())
-        .then(data => {
-          if (!data.latitude || !data.longitude) return;
-
-          const position = {
-            lat: parseFloat(data.latitude),
-            lng: parseFloat(data.longitude)
-          };
-
-          marker.setPosition(position);
-          map.setCenter(position);
-        });
-    }
-  </script>
-
-  <script async
-    src="https://maps.googleapis.com/maps/api/js?key=&callback=initMap">
-  </script>
-</body>
+ <body>
+    <div class="row">
+      <div class="title">Updated</div>
+      <div class="data" id="date"></div>
+    </div>
+    <div class="row">
+      <div class="title">Latitude, Longitude</div>
+      <div class="data">
+        <span id="lat"></span>, <span id="lng"></span>
+      </div>
+    </div>
+  </body>
 </html>
